f5dbe69e1b8caa902cd0d6fe9c142de5d8ce41d0
Abdul Wahab <abdulwahab@Abduls-MacBook-Pro-311.local>
2020-04-14 11:57:33 +0200

    component updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import "./App.css";
 import NavBar from "./Components/navbar";
 import Counters from "./Components/counters";
@@ -10,15 +9,15 @@ class App extends Component {
       { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
+      { id: 4, value: 0 },
+    ],
   };
-  handleDelete = counterId => {
+  handleDelete = (counterId) => {
     //console.log("Event handleer Called", counterId);
-    const counters = this.state.counters.filter(c => c.id !== counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
   };
-  handleIncrement = counter => {
+  handleIncrement = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
@@ -27,7 +26,7 @@ class App extends Component {
     //console.log(this.state.counters[index]);
   };
   handleReset = () => {
-    const counters = this.state.counters.map(c => {
+    const counters = this.state.counters.map((c) => {
       c.value = 0;
       return c;
     });
@@ -37,7 +36,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <NavBar
-          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
         />
         <main className="container">
           <Counters
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 
 class NavBAr extends Component {
-  mystyle = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
-    padding: "10px",
-    fontFamily: "Arial"
-  };
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand">
-          <h2 style={this.mystyle}>Counter Application</h2>{" "}
-          <span className="badge badge-pill badge-secondry">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
             Total Count : {this.props.totalCounters}
           </span>
         </a>
