fae3673fd4ea0ed9216cc75628cfb249c754bb4b
Abdul Wahab <abdulwahab@Abduls-MacBook-Pro-311.local>
2020-04-14 12:03:52 +0200

    Updates

---
+++ precache-manifest.40e5c07ad769f4357166609482591882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc1fbb8806fff92e4f4d5765d932340",
+    "url": "/Counter-React-App/index.html"
+  },
+  {
+    "revision": "469bdf41a9c81f8da46e",
+    "url": "/Counter-React-App/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "7d6118016ffa47409e2f",
+    "url": "/Counter-React-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "469bdf41a9c81f8da46e",
+    "url": "/Counter-React-App/static/js/2.62ed2149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter-React-App/static/js/2.62ed2149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6118016ffa47409e2f",
+    "url": "/Counter-React-App/static/js/main.105addea.chunk.js"
+  },
+  {
+    "revision": "9e412cae8104931f3115",
+    "url": "/Counter-React-App/static/js/runtime-main.82cb67a5.js"
+  }
+]);
